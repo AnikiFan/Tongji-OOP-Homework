388520b5a51ace4ce104b8f68369be3028abf4ee
AnikiFan <15800788661@163.com>
2023-11-13 17:32:58 +0800

    Initialize

--- test/TEST/cpp_project_for_normal_homework1/test.cpp
+++ test/TEST/cpp_project_for_normal_homework1/test.cpp
@@ -2,7 +2,9 @@
 #define MAXN 11000
 #define OK 1
 #define ERROR 0
-#define MAXINFOLENGTH 30
+#define MAXINFOLENGTH 20
+#define OVERFLOW {printf("\n\nOVERFLOW\n\n");return -2;}
+#include<stack>
 #include<stdio.h>
 #include<cstdlib>
 typedef int Status;
@@ -12,13 +14,14 @@ typedef struct {
 	int rchild;
 }ElemType;
 typedef struct {
-	ElemType elem[MAXN];
+	ElemType* elem;
 	int root;
 } Tree;
 Status CreateTree(Tree& T)
 {
 	int n;
 	char temp[MAXINFOLENGTH];
+	int mark[MAXN] = { 0 };
 	scanf("%d", &n);
 	getchar();
 	for (int i = 0; i < n; i++) {
@@ -27,95 +30,91 @@ Status CreateTree(Tree& T)
 		scanf("%s", temp);
 		if (temp[0] == '-')
 			T.elem[i].lchild = -1;
-		else
+		else {
 			T.elem[i].lchild = atoi(temp);
+			mark[T.elem[i].lchild] = 1;
+		}
 		scanf("%s", temp);
 		if (temp[0] == '-')
 			T.elem[i].rchild = -1;
-		else
+		else {
 			T.elem[i].rchild = atoi(temp);
+			mark[T.elem[i].rchild] = 1;
+		}
 	}
-	int valid;
-	for (int i = 0; i < n; i++) {
-		valid = 1;
-		for (int j = 0; j < n; j++)
-			if (i != j && (T.elem[j].rchild == i || T.elem[j].lchild == i)) {
-				valid = 0;
-				break;
-			}
-		if (valid) {
+	for (int i = 0; i < n; i++)
+		if (!mark[i]) {
 			T.root = i;
 			return OK;
 		}
-	}
 	return ERROR;
 }
-int Depth(Tree T, int root)
+int Depth(Tree T)
 {
-	if (root == -1)
-		return 0;
-	else
-		return Depth(T, T.elem[root].lchild) > Depth(T, T.elem[root].rchild) ? Depth(T, T.elem[root].lchild) + 1 : Depth(T, T.elem[root].rchild) + 1;
+	std::stack<int>s1, s2;
+	int p = T.root;
+	int depth = 1;
+	int maxdepth = 1;
+	while (!s1.empty() || p != -1) {
+		while (p != -1) {
+			s1.push(p);
+			p = T.elem[p].lchild;
+
+			s2.push(depth);
+			depth++;
+		}
+		p = s1.top();
+		s1.pop();
+		p = T.elem[p].rchild;
+
+		if (p != -1)
+			depth = s2.top() + 1;
+		else
+			depth = s2.top();
+		s2.pop();
+		if (depth > maxdepth)
+			maxdepth = depth;
+
+	}
+	return maxdepth;
 }
 Status valid(Tree T1, int root1, Tree T2, int root2)
 {
-	if (T1.elem[root1].lchild == -1
-		|| T2.elem[root2].lchild == -1
-		|| T1.elem[root1].rchild == -1
-		|| T2.elem[root2].rchild == -1) {
-		if (T1.elem[root1].lchild == -1
-			&& T2.elem[root2].lchild == -1
-			&& T1.elem[root1].rchild == -1
-			&& T2.elem[root2].rchild == -1
-			&& T1.elem[root1].data == T2.elem[root2].data)
-			return OK;
-		else if (T1.elem[root1].lchild == -1 && T2.elem[root2].lchild == -1 &&
-			T1.elem[root1].rchild != -1 && T2.elem[root2].rchild != -1
-			&& T1.elem[root1].data == T2.elem[root2].data)
-			return valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].rchild);
-		else if (T1.elem[root1].lchild != -1 && T2.elem[root2].lchild != -1 &&
-			T1.elem[root1].rchild == -1 && T2.elem[root2].rchild == -1
-			&& T1.elem[root1].data == T2.elem[root2].data)
-			return valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].lchild);
-		else if (T1.elem[root1].lchild != -1 && T2.elem[root2].lchild == -1 &&
-			T1.elem[root1].rchild == -1 && T2.elem[root2].rchild != -1
-			&& T1.elem[root1].data == T2.elem[root2].data)
-			return valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].rchild);
-		else if (T1.elem[root1].lchild == -1 && T2.elem[root2].lchild != -1 &&//都为叶子结点
-			T1.elem[root1].rchild != -1 && T2.elem[root2].rchild == -1
-			&& T1.elem[root1].data == T2.elem[root2].data)
-			return valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].lchild);
-		else
-			return ERROR;
-	}
-	else if (T1.elem[root1].lchild == T2.elem[root2].lchild && T1.elem[root1].rchild == T2.elem[root2].rchild
-		&& T1.elem[root1].lchild != T1.elem[root1].rchild)//左右子树对应相等，且左右子树互不相等
-		return (valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].lchild)
-			&& valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].rchild));
-	else if (T1.elem[root1].lchild == T2.elem[root2].rchild && T1.elem[root1].rchild == T2.elem[root2].lchild
-		&& T1.elem[root1].lchild != T1.elem[root1].rchild)//左右子树交错相等，且左右子树互不相等
-		return (valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].rchild)
-			&& valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].lchild));
-	else if (T1.elem[root1].lchild == T2.elem[root2].rchild && T1.elem[root1].rchild == T2.elem[root2].lchild
-		&& T1.elem[root1].lchild == T1.elem[root1].rchild)//四个子树都相等
-		return((valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].rchild)
-			&& valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].lchild))
-			|| (valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].lchild)
-				&& valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].rchild)));
+	if (root1 == -1 && root2 == -1)
+		return OK;
+	else if (root1 == -1 && root2 != -1 || root1 != -1 && root2 == -1)
+		return ERROR;
+	else if (T1.elem[root1].data != T2.elem[root2].data)
+		return ERROR;
 	else
-		return 0;
+		return (valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].lchild) && valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].rchild)
+			|| valid(T1, T1.elem[root1].lchild, T2, T2.elem[root2].rchild) && valid(T1, T1.elem[root1].rchild, T2, T2.elem[root2].lchild));
+}
+Status InitTree(Tree& T)
+{
+	T.elem = (ElemType*)malloc(sizeof(ElemType) * MAXN);
+	if (!T.elem)
+		OVERFLOW;
+	for (int i = 0; i < MAXN; i++)
+		T.elem[i].data = -1;
+	return OK;
 }
 
 int main()
 {
 	Tree T1, T2;
+	InitTree(T1);
+	InitTree(T2);
 	CreateTree(T1);
-	printf("find root: %d\n", T1.root);
 	CreateTree(T2);
-	printf("find root: %d\n", T2.root);
-	printf("valid: %d\n", valid(T1, T1.root, T2, T2.root));
-	printf("%d\n", Depth(T1, T1.root));
-	printf("%d\n", Depth(T2, T2.root));
+	//for(int i = 0;i<6;i++)
+	  //printf("%d %d %d\n",T1.elem[i].data,T1.elem[i].lchild,T1.elem[i].rchild);
+	if (valid(T1, T1.root, T2, T2.root))
+		printf("Yes\n");
+	else
+		printf("No\n");
+	printf("%d\n", Depth(T1));
+	printf("%d\n", Depth(T2));
 	return 0;
 }
 
